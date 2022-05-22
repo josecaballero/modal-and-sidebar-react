dc0a82c6f553b54a1c2d4ca2943ec7c435a674a5
Jose <jlcab8@gmail.com>
2022-05-22 12:52:49 +0200

    implement useContext and use it to toggle sidebar and modal components

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,15 @@ import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 
 const Home = () => {
-  const data = useGlobalContext();
-  console.log(data);
+  const { openSidebar, openModal } = useGlobalContext();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button onClick={openSidebar} className="sidebar-toggle">
         <FaBars />
       </button>
-      <button className="btn">show modal</button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
     </main>
   );
 };
